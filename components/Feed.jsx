b37e3f35350c0286938b4e766495ba6acc1637e4
Rakesh <krakesh7788@gmail.com>
2024-02-11 17:08:22 +0530

    first commit

---
+++ components/Feed.jsx
@@ -0,0 +1,101 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import PromptCard from "./PromptCard"
+import Image from "next/image"
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  )
+}
+
+const Feed = () => {
+  const [searchText, setSearchText] = useState("")
+  const [allPosts, setAllPosts] = useState([])
+  const [searchTimeout, setSearchTimeout] = useState(null)
+  const [searchedResults, setSearchedResults] = useState([])
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout)
+    setSearchText(e.target.value)
+
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value)
+        setSearchedResults(searchResult)
+      }, 500)
+    )
+  }
+
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchtext, "i") // 'i' flag for case-insensitive search
+    return allPosts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    )
+  }
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName)
+
+    const searchResult = filterPrompts(tagName)
+    setSearchedResults(searchResult)
+  }
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/prompt")
+      const data = await response.json()
+      setAllPosts(data)
+    }
+
+    fetchPosts()
+  }, [])
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+        {searchText && (
+          <Image
+            alt="clear_icon"
+            src="/assets/icons/cross2.svg"
+            width={25}
+            height={25}
+            className="absolute right-2 cursor-pointer"
+            onClick={() => setSearchText("")}
+          />
+        )}
+      </form>
+
+      {searchText ? (
+        <PromptCardList
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        />
+      ) : (
+        <PromptCardList data={allPosts} handleTagClick={handleTagClick} />
+      )}
+    </section>
+  )
+}
+
+export default Feed
